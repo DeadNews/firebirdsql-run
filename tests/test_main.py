25627f6fe9a8e4f6e1a8d5174c03a76f2c9493de
DeadNews <uhjnnn@gmail.com>
2022-07-21 17:58:52 +0200

    Add tests Add coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python
-from os import environ
+import pytest
 
-from src.firebirdsql_run import getpw
+from src.firebirdsql_run import callproc, connection, execute
 
 
-def test_getpw():
-    pw = "pbbmaDXpeNKJ7iMS475qnqvCYsymjZ"
-    environ["FB_PASSWD"] = pw
+def test_connection():
+    with pytest.raises(ConnectionRefusedError):
+        connection(
+            host="localhost",
+            db="fdb",
+            port=3050,
+            user="sysdba",
+            passwd="masterkey",
+        )
 
-    assert getpw() == pw
+
+def test_execute():
+    result = execute(
+        query="SELECT * FROM table",
+        host="localhost",
+        db="fdb",
+        user="sysdba",
+        passwd="masterkey",
+    )
+
+    assert result.host == "localhost"
+    assert result.db == "fdb"
+    assert result.user == "sysdba"
+    assert result.returncode == 1
+    assert len(result.error) > 0
+    assert result.query == "SELECT * FROM table"
+    assert result.params == ()
+    assert result.data == []
+
+
+def test_callproc():
+    result = callproc(
+        procname="PROCNAME",
+        params=("p1", "p2", "p3"),
+        host="localhost",
+        db="fdb",
+        user="sysdba",
+        passwd="masterkey",
+    )
+
+    assert result.host == "localhost"
+    assert result.db == "fdb"
+    assert result.user == "sysdba"
+    assert result.returncode == 1
+    assert len(result.error) > 0
+    assert result.query == "EXECUTE PROCEDURE PROCNAME ?,?,?"
+    assert result.params == ("p1", "p2", "p3")
+    assert result.data == []
