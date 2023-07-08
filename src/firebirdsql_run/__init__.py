83f4fbba59f6db8d18a41f415b6b0b6dcb43c7da
DeadNews <uhjnnn@gmail.com>
2023-07-08 16:44:56 +0530

    fix: move connection close to `finally` block (#78)

--- src/firebirdsql_run/__init__.py
+++ src/firebirdsql_run/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 """Public accessible objects of that module."""
 
-from firebirdsql_run.main import Connection, ExecuteError, callproc, connect, connection, execute
-from firebirdsql_run.type import CompletedTransaction, Dataset, FBTypes
+from firebirdsql_run.main import Connection, callproc, connect, connection, execute
+from firebirdsql_run.type import CompletedTransaction, Dataset, ExecuteError, FBTypes
